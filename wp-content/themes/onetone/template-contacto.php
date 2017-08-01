a6704fb650551440793545fc616b2992c3b83edf
melmiranda <melania.smc@hotmail.com>
2017-08-01 14:16:04 -0300

    14:15

--- wp-content/themes/onetone/template-contacto.php
+++ wp-content/themes/onetone/template-contacto.php
@@ -11,5 +11,24 @@
 get_header();
 ?>
 
+<style media="screen">
+  .left-contacto{
+    background-color:
+  }
+</style>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-contacto">
+  <div class="">
+
+  </div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 left-contacto">
+
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 right-contacto">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.5967731819333!2d-58.4386010847715!3d-34.56376348046975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5b9dbca9c35%3A0xb42d51fcdb4b378b!2sAv.+del+Libertador+4992%2C+C1426BWX+CABA!5e0!3m2!1ses!2sar!4v1501607585155" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
 
 <?php get_footer();?>
