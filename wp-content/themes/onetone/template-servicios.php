d03bef4fc79f0366a39c6c754140b47b1d2c87d7
melmiranda <melania.smc@hotmail.com>
2017-08-02 13:28:06 -0300

    13:28

--- wp-content/themes/onetone/template-servicios.php
+++ wp-content/themes/onetone/template-servicios.php
@@ -10,21 +10,227 @@
 
 get_header();
 ?>
+<style media="screen">
+  .header-servicios{
+    height: 500px;
+    /*height: 300px;*/
+    background-image: url(<?php echo site_url(); ?>/wp-content/themes/onetone/img/Servicios_Banner.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-bottom: 0;
+  }
 
+  .body-servicios{
+    background-color: black;
+    background-image: url(<?php echo site_url(); ?>/wp-content/themes/onetone/img/Servicios_Main.jpg);
+    background-position: top;
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    padding-top: 10%;
+    padding-bottom: 10%;
+    margin-bottom: 0;
+  }
+
+
+    .float-servicios{
+      position: absolute;
+      width: 90%;
+      left: 0;
+      right: 0;
+      margin:auto;
+      height: 200px;
+      background-color: #008E8E;
+      top:-100px;
+      text-align: center;
+      color:white;
+      font-size: 1.4em;
+      font-family: 'Roboto-medium';
+      padding-top: 30px;
+    }
+
+    .float-servicios p:first-child{
+      font-family: 'Roboto-black';
+      font-size: 15px;
+      color:#004851;
+    }
+    .informacion-manos{
+      padding: 20px 20%;
+      color:white;
+    }
+
+    .informacion-manos h2{
+      font-family: 'Roboto-bold';
+      font-size: 2.5em;
+      color:white;
+      margin-bottom: 20px;
+    }
+
+    .informacion-manos h2 span{
+      font-family: 'Roboto-thin';
+      font-size: 25px;
+    }
+
+    .secciones-mano{
+      height: 250px;
+      /*border: 1px solid #008E8E;*/
+    }
+
+    .contenedor-mano-descripcion{
+      background-color: rgba(255,255,255,.2);
+      padding: 0px 20px 0px 50px;
+      max-width: 400px;
+      float:right;
+      position: relative;
+    }
+
+    .contenedor-mano-descripcion p.title-mano{
+      color:#D5D5D6 ;
+      font-family:'Roboto-black';
+      margin-left: 20px;
+      margin-top: 10px;
+    }
+
+    .contenedor-mano-descripcion p{
+      color:white;
+      font-size: 1em;
+      line-height: 20px;
+    }
+
+
+
+    .contenedor-mano-descripcion p span{
+     color:#008E8E;
+     font-family:'Roboto-black'
+     }
+
+    .circle-mano{
+      width: 35px;
+      height: 35px;
+      border: 8px solid #008E8E;
+      border-radius: 100%;
+      background-color: white;
+      position: absolute;
+      left: 0;
+      top:0;
+      margin-left: 25px;
+      margin-top: 25px;
+    }
+
+
+    .circle-mano-r{
+      width: 35px;
+      height: 35px;
+      border: 8px solid #008E8E;
+      border-radius: 100%;
+      background-color: white;
+      position: absolute;
+      right: 0;
+      top:0;
+      margin-right: 25px;
+      margin-top: 25px;
+    }
+</style>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-servicios">
 
 </div>
 
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color:black; margin-bottom:0; height:200px;">
+  <div class="float-servicios" style=" z-index:3">
+    <p>Servicios</p>
+    <p>Las afecciones del miembro superior son muy frecuentes y pueden alterar <br>tus capacidades. Nosotros lo sabemos y te podemos ayudar a resolverlo. <br>Lo hacemos en forma integral y lo hacemos juntos.</p>
+  </div>
+</div>
+
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 body-servicios">
 
-  <div class="float-servicios">
 
-  </div>
-  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-
-  </div>
 
   <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 secciones-mano" style="height: 500px;">
+      <div class="contenedor-mano-descripcion">
+        <div class="circle-mano"></div> <p <p class="title-mano">HOMBRO</p>
+          <p><span>Procedimientos</span><br>Artroscopía, reconstrucciones ligamentarias,
+            transferencias tendinosas, reemplazos
+            protésicos, tratamiento del manguito rotador
+            y la luxación recidivante.</p>
+          <p><span>Patologias frecuentes</span><br>Fractura de clavícula, fractura de húmero,
+            luxación de hombro, fracturas de escápula,
+            lesiones del plexo braquial.</p>
+      </div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 secciones-mano" style="height: 400px;">
+      <div class="contenedor-mano-descripcion">
+        <div class="circle-mano"></div> <p class="title-mano">CODO</p>
+          <p><span>Procedimientos</span><br>Artroscopía, reconstrucciones ligamentarias,
+            transferencias tendinosas, reemplazos
+            protésicos, tratamiento del manguito rotador
+            y la luxación recidivante.</p>
+          <p><span>Patologias frecuentes</span><br>Fractura de clavícula, fractura de húmero,
+            luxación de hombro, fracturas de escápula,
+            lesiones del plexo braquial.</p>
+      </div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 secciones-mano">
+      <div class="contenedor-mano-descripcion">
+        <div class="circle-mano-r" ></div> <p <p class="title-mano">HOMBRO</p>
+          <p><span>Procedimientos</span><br>Artroscopía, reconstrucciones ligamentarias,
+            transferencias tendinosas, reemplazos
+            protésicos, tratamiento del manguito rotador
+            y la luxación recidivante.</p>
+          <p><span>Patologias frecuentes</span><br>Fractura de clavícula, fractura de húmero,
+            luxación de hombro, fracturas de escápula,
+            lesiones del plexo braquial.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 informacion-manos">
+      <h2>Excelencia y calidad <br><span>para el diagnóstico y tratamiento</span></h2>
+
+      <p>Para lograr el diagnóstico correcto, la patología
+        del miembro superior requiere de un alto grado
+        de especialización. Su tratamiento quirúrgico
+        necesita de profesionales con habilidad y
+        delicadeza suficientes para resolver tareas de
+        alta complejidad.
+        Aquellas personas que necesitan un alto nivel de
+        rendimiento en su miembro superior y buscan
+        una atención cuidadosa y personalizada,
+        encontrarán en Synectia el mejor aliado. Entre
+        otras herramientas innovadoras, realizamos
+        artroscopías en pequeñas articulaciones y
+        diferentes técnicas mínimamente invasivas.
+        En el caso específico de la mano, manejamos la
+        complejidad de este tipo de cirugías
+        programando el tratamiento de sus diferentes
+        tejidos de manera integral, siempre enfocados
+        en lograr mayor funcionalidad y sin perder de
+        vista la importancia del aspecto estético. </p>
+
+    </div>
+
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 secciones-mano" style="position:absolute; bottom:0; right:0">
+      <div class="contenedor-mano-descripcion" style="float:left" >
+        <div class="circle-mano" ></div> <p <p class="title-mano">HOMBRO</p>
+          <p><span>Procedimientos</span><br>Artroscopía, reconstrucciones ligamentarias,
+            transferencias tendinosas, reemplazos
+            protésicos, tratamiento del manguito rotador
+            y la luxación recidivante.</p>
+          <p><span>Patologias frecuentes</span><br>Fractura de clavícula, fractura de húmero,
+            luxación de hombro, fracturas de escápula,
+            lesiones del plexo braquial.</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background:black; height:200px;">
 
   </div>
 </div>
